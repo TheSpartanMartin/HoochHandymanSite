048ad7ca7a0d9763b0866dbc5e7fbea6933c9560
David Martin <davidemartin854@gmail.com>
2026-01-06 11:28:06 -0500

    Update HoochHandymanSite.tsx

--- src/HoochHandymanSite.tsx
+++ src/HoochHandymanSite.tsx
@@ -420,20 +420,7 @@ export default function HoochHandymanSite() {
                   type="submit"
                   form="estimate-form"
                   className="rounded-2xl px-5 py-3 text-sm font-semibold text-white"
-                  style={{ background: COLORS.primary }}
-                  onClick={() => {
-                    console.log("Request Estimate clicked");
-                    const form = document.getElementById("estimate-form");
-                    console.log("Form found?", !!form);
-
-                    if (form && "requestSubmit" in form) {
-                      form.requestSubmit();
-                    } else if (form) {
-                      form.submit()
-                    } else {    
-                      alert('Form not found. Make sure your <form> has id="estimate-form".');
-                    }
-                  }}  
+                  style={{ background: COLORS.primary }}                      
                 >
                   Request Estimate
                 </button>
@@ -669,9 +656,4 @@ function FacebookIcon({ className = "h-4 w-4" }) {
     </svg>
   );
 }
-function MapPinIcon() {
-  return null;
-}
-function ShieldCheckIcon() {
-  return null;
-}
+
